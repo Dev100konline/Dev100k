5c374f5566bd407ff9fc217b4d5810a5fba36eb1
Dev100konline <kattapavankalyan97@gmail.com>
2025-12-10 23:02:20 +0530

    test

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -2,3 +2,5 @@
 
 
 echo "hello world first program"
+
+echo "Shell is powerfull"
---
+++ 02-variables.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+NUM1=$2
+NUM2=$1
+PERSON1=Ram
+
+echo $PERSON1 "first person"
+echo "please enter PIN"
+read -s PIN
+read PIN2
+
+echo "you pin is $PIN"
+
+echo $NUM1
+echo $NUM2
+
+echo $NUM1 + $NUM2
+SUM=$(($NUM1 + $NUM2))  #adding 2 numbers
+
+echo $SUM
+
+echo "done for the day"
+echo "variables file"
+
