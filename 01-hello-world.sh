2a560e29bb6a49ff802c22446f43226b1a8f7363
Dev100konline <kattapavankalyan97@gmail.com>
2025-12-10 22:58:25 +0530

    test

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
 
-per1=$2
-per2=$1
 
-echo $person1 "first person"
-echo "please enter PIN"
-read pin
-echo "you pin is $pin"
-
-echo $per1
-echo $per2
-
-echo $per1 + $per2
-sum=$(($per1 + $per2))
-echo $sum
-echo "done for the day"
-
+echo "hello world first program"
